594bad2c8ea3875d40d6cdc2c7cb8d457578ac58
James Chou <james7800243@users.noreply.github.com>
2017-01-05 13:43:57 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    res = makeWebhookResult(req)
+    return res
+
+
+def makeWebhookResult(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+	indicator = parameters.get("Indicators")
+	
+	speech = "Your indoor " + indicator + "is "
+	
+	if indicator == "temperature":
+	speech += "23 Celsius degree, it's very comfortable now."
+	
+	else if indicator == "co2":
+	speech += "higher than 1000, please open the window and turn on the air purifier."
+	
+	else:
+	speech += "good, don't worry."
+		
+    # print(json.dumps(item, indent=4))
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "airmonitor-webhook-sample-for-apiai"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=False, port=port, host='0.0.0.0')
